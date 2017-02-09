12b1656a409e42a63b95b7185bb2d9824f2d03a3
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-02-09 12:08:42 +0100

    DB Set-up

---
+++ app/models/ingredient.rb
@@ -0,0 +1,16 @@
+class Ingredient < ApplicationRecord
+  has_many :doses
+
+  validates :name, presence: true, uniqueness: true
+
+  before_destroy :check_for_cocktails
+
+  private
+
+  def check_for_cocktaiks
+    if doses.any?
+      erros[:base] << "Cannot delete ingredient used by a cocktail"
+      return false
+    end
+  end
+end
