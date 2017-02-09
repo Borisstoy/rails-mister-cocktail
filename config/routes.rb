f76214e7b649f5da09080f7e7ec74aaa3575d735
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-02-09 13:57:16 +0100

    Routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'cocktails#index'
 
   resources :cocktails do
-    resources :doses, only; [:new, :create]
+    resources :doses, only: [:new, :create]
   end
 
 end
