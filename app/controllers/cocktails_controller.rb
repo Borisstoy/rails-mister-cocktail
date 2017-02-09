616dec85e5cc5abb5aaff920c837f25bf9cad5c9
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-02-09 13:56:16 +0100

    DB + Routes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,19 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_cocktails
+
+  end
+
+  def cocktails_params
+
+  end
 end
