5fca0bb304b0cd7db93b2face41963222abdca0a
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-02-10 16:17:36 +0100

    Seed cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,25 @@
 require 'json'
 require 'open-uri'
 
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-ingredients_serialized = open(url).read
-ingredients = JSON.parse(ingredients_serialized)
+### INGREDIENTS
 
-ingredients['drinks'].each do |ing|
-  Ingredient.create(name: "#{ing.values.first}")
+# Ingredient.destroy_all
+
+# url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# ingredients_serialized = open(url).read
+# ingredients = JSON.parse(ingredients_serialized)
+
+# ingredients['drinks'].each do |ing|
+#   Ingredient.create(name: "#{ing.values.first}")
+# end
+
+### RECIPES
+
+recipes_serialized = File.read('db/recipes.json')
+recipes = JSON.parse(recipes_serialized)
+
+recipes.each do |recipe|
+  Cocktail.create(name: "#{recipe.values.first}")
 end
+
+
